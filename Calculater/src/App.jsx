f585646db0b12d9c288476c7697918873c4abb96
Rishabh Pal <174471886+Rishp-3@users.noreply.github.com>
2026-04-09 18:46:48 +0530

    ok

---
+++ Calculater/src/App.jsx
@@ -0,0 +1,19 @@
+import Button from "./components/Button";
+const App = () => {
+  return (
+    <div>
+      <Button number="1"/>
+      <Button number="2"/>
+      <Button number="3"/>
+      <Button number="4"/>
+      <Button number="5"/>
+      <Button number="6"/>
+      <Button number="7"/>
+      <Button number="8"/>
+      <Button number="9"/>
+      <Button number="0"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Calculater/src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button (props) {
+    return (
+        <div>
+            <button>{props.number}</button>
+        </div>
+    );
+}
+
+export default Button;
