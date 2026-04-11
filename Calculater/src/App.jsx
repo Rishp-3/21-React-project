2a7dad2bff74bebe9f043cd502b9674f09b57f36
Rishabh Pal <174471886+Rishp-3@users.noreply.github.com>
2026-04-11 19:33:05 +0530

    update

--- Calculater/src/App.jsx
+++ Calculater/src/App.jsx
@@ -1,5 +1,6 @@
+import React, { useState } from "react";
 import "./App.css";
-const App = () => {
+function App() {
   return (
     <div className="container">
       <div className="ansQue">
@@ -7,28 +8,28 @@ const App = () => {
         <div className="que"></div>
       </div>
       <div className="button">
-        <button className="c1" value="C">C</button>
-        <button className="c1" value="⌫">⌫</button>
-        <button className="c1" value="%">%</button>
-        <button className="c2" value="/">/</button>
-        <button value="7">7</button>
-        <button value="8">8</button>
-        <button value="9">9</button>
-        <button className="c2" value="X">X</button>
-        <button value="4">4</button>
-        <button value="5">5</button>
-        <button value="6">6</button>
-        <button className="c2" value="-">-</button>
-        <button value="1">1</button>
-        <button value="2">2</button>
-        <button value="3">3</button>
-        <button className="c2" value="+">+</button>
-        <button className="ze" value="0">0</button>
-        <button value=".">.</button>
-        <button className="c3" value="=">=</button>
+        <button className="c1">C</button>
+        <button className="c1">⌫</button>
+        <button className="c1">%</button>
+        <button className="c2">/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="c2">X</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className="c2">-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className="c2">+</button>
+        <button className="ze">0</button>
+        <button>.</button>
+        <button className="c3">=</button>
       </div>
     </div>
   );
-};
+}
 
 export default App;
